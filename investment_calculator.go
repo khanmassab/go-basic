f4b1bc8712c9106243cf20872ac79f55e5ecf6f9
Massab <massab725@gmail.com>
2024-11-22 19:41:33 +0500

    User Experience

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,12 +6,22 @@ import (
 )
 
 func main() {
-	var investmentAmount float64   = 1000
-	var expectedReturnRate float64 = 5.5
-	var years float64                  = 10
+  const inflationRate = 2.5
+	var investmentAmount, years, expectedReturnRate float64
+
+  fmt.Print("Enter Investment Amount: ")
+  fmt.Scan(&investmentAmount)
+
+  fmt.Print("Enter Number of Years: ")
+  fmt.Scan(&years)
+
+  fmt.Print("Enter Expected Return Rate: ")
+  fmt.Scan(&expectedReturnRate)
 
   //Formula
-	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
+  futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years) 
+  futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years)
 
-  fmt.Print(futureValue)
+  fmt.Println(futureValue)
+  fmt.Println(futureRealValue)
 }
