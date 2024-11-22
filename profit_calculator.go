99feaf0d953060c56ca96cc2146f18111b6b1df4
Massab <massab725@gmail.com>
2024-11-22 19:55:50 +0500

    Output

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
+	var revenue, expense, taxRate float64
 	
+	//Getting Inputs
+	fmt.Print("Enter Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter Expense: ")
+	fmt.Scan(&expense)
+
+	fmt.Print("Enter Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	//Calculation
+	earningBeforeTax   := revenue - expense
+	earningAfterTax    := earningBeforeTax * (1 - taxRate/100)
+	ratio := earningBeforeTax / earningAfterTax
+
+	//Printing Output
+	fmt.Println(earningBeforeTax)
+	fmt.Println(earningAfterTax)
+	fmt.Println(ratio)
 }
