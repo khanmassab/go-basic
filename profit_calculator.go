532eb9d5285dc5ede9aa12a09778827ccfc778d3
Massab <massab725@gmail.com>
2024-11-23 19:56:19 +0500

    Functions Exercise

--- profit_calculator.go
+++ profit_calculator.go
@@ -6,23 +6,27 @@ func main() {
 	var revenue, expense, taxRate float64
 	
 	//Getting Inputs
-	fmt.Print("Enter Revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Enter Expense: ")
-	fmt.Scan(&expense)
-
-	fmt.Print("Enter Tax Rate: ")
-	fmt.Scan(&taxRate)
+	revenue = printAndScan("Enter Revenue: ", revenue)
+	expense = printAndScan("Enter Expense: ", expense)
+	taxRate = printAndScan("Enter Tax Rate: ", taxRate)
 
 	//Calculation
-	earningBeforeTax   := revenue - expense
-	earningAfterTax    := earningBeforeTax * (1 - taxRate/100)
-	ratio              := earningBeforeTax / earningAfterTax
+	earningBeforeTax, earningAfterTax, ratio   := calculateValues(revenue, expense, taxRate)
 
 	//Printing Output
-	fmt.Printf("Your earnings before tax is: %v\nand your earnings after tax (profit) is: %v\nwhereas the profit to earning ratio is: %v", earningBeforeTax, earningAfterTax, ratio)
-	// fmt.Println("Earnings before tax: ", earningBeforeTax)
-	// fmt.Println("Earnings after tax: ", earningAfterTax)
-	// fmt.Println(ratio)
+	fmt.Printf("Your earnings before tax is: %v\nand your earnings after tax (profit) is: %v\nwhereas the profit to earning ratio is: %f", earningBeforeTax, earningAfterTax, ratio)
+}
+
+func printAndScan(text string, numberToScan float64) (float64) {
+	fmt.Print(text)
+	fmt.Scan(&numberToScan)
+	return numberToScan
+}
+
+func calculateValues (revenue, expense, taxRate float64) (earningBeforeTax float64, earningAfterTax float64, ratio float64) {
+	earningBeforeTax   = revenue - expense
+	earningAfterTax    = earningBeforeTax * (1 - taxRate/100)
+	ratio              = earningBeforeTax / earningAfterTax
+
+	return
 }
