1f5c6cc05071bb80fccc735c9209bbb56b167da6
Massab <massab725@gmail.com>
2024-11-25 11:36:28 +0500

    Last exercise with Sprintf

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,20 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+const valuesFile = "values.txt"
 
 func main() {
 	var revenue, expense, taxRate float64
 	
 	//Getting Inputs
 	revenue = printAndScan("Enter Revenue: ", revenue)
-	expense = printAndScan("Enter Expense: ", expense)
-	taxRate = printAndScan("Enter Tax Rate: ", taxRate)
+	revenue, err := validateInput(revenue)
 
+	if err != nil {
+		panic(err)
+	}
+
+	expense = printAndScan("Enter Expense: ", expense)
+	expense, err = validateInput(expense)
+
+	if err != nil {
+		panic(err)
+	}
+
+	taxRate = printAndScan("Enter Tax Rate: ", taxRate)
+	taxRate, err = validateInput(taxRate)
+
+	if err != nil {
+		panic(err)
+	}
+	
 	//Calculation
 	earningBeforeTax, earningAfterTax, ratio   := calculateValues(revenue, expense, taxRate)
 
+	//Storing Output
+	formattedOutput := fmt.Sprintf("Your earnings before tax is: %v\nand your earnings after tax (profit) is: %v\nwhereas the profit to earning ratio is: %.2f", earningBeforeTax, earningAfterTax, ratio)
+	storeValue(formattedOutput)
+
 	//Printing Output
-	fmt.Printf("Your earnings before tax is: %v\nand your earnings after tax (profit) is: %v\nwhereas the profit to earning ratio is: %.2f", earningBeforeTax, earningAfterTax, ratio)
+	fmt.Printf(formattedOutput)
 }
 
 func printAndScan(text string, numberToScan float64) (float64) {
@@ -29,4 +56,16 @@ func calculateValues (revenue, expense, taxRate float64) (earningBeforeTax float
 	ratio              = earningBeforeTax / earningAfterTax
 
 	return
+}
+
+func validateInput(input float64) (float64, error){
+	if input <= 0 {
+		err := errors.New("invalid input! number should be greater than zero")
+		return input, err
+	} 
+	return input, nil
+}
+
+func storeValue(value string){
+	os.WriteFile(valuesFile, []byte(value), 0641)
 }
