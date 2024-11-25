0dd0f95e0f684fc5b76d322f531188f8bcc97dba
Massab <massab725@gmail.com>
2024-11-26 00:23:44 +0500

    constructor validation Scanln instead of Scan if an empty input is needed

--- structs.go
+++ structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -23,13 +24,16 @@ func (u *user) clearUserName (){
 	u.lastName = ""
 }
 
-func newUser(firstName, lastName, birthDate string) *user {
+func newUser(firstName, lastName, birthDate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("First name, Last name and Birthdate are required")
+	}
 	return &user{
 		firstName: firstName,
 		lastName: lastName,
 		birthDate:birthDate,
 		createdAt: time.Now(),
-	}
+	}, nil
 }
 
 func main() {
@@ -37,7 +41,13 @@ func main() {
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser *user = newUser(userFirstName, userLastName, userBirthdate)
+	var appUser *user
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	appUser.outputUserData()
 	appUser.clearUserName()
@@ -47,6 +57,6 @@ func main() {
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
