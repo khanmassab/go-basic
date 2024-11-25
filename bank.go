4640386250dd27954a5a344d3dc91e10fb6877a2
Massab <massab725@gmail.com>
2024-11-25 09:35:07 +0500

    decisons and nested decisons

---
+++ bank.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main (){
+	var choice int
+	amount   := 100.0
+
+	fmt.Println("Welcome to Go Bank")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check Balance")
+	fmt.Println("2. Make Deposit")
+	fmt.Println("3. Withdraw Amount")
+	fmt.Println("4. Exit")
+
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	if choice == 1{
+		fmt.Print("Your balance is: ", amount)
+	} else if choice == 2 {
+		var deposit float64
+		fmt.Print("Enter the amount you want to deposit: ")
+		fmt.Scan(&deposit)
+
+		if deposit <= 0 {
+			fmt.Print("Invalid Input")
+			return
+		}
+
+		amount  += deposit
+		fmt.Print("Deposit successful! Your new is balance is: ", amount)
+	} else if  choice == 3 {
+		var withdrawalAmount float64
+		fmt.Print("Enter the amount you want to withdraw: ")
+		fmt.Scan(&withdrawalAmount)
+
+		if withdrawalAmount > amount {
+			fmt.Print("Error! Your balance is low")
+			return
+		} 
+
+		if withdrawalAmount < 0 {
+			fmt.Print("Invalid amount!")
+			return
+		}
+
+		amount -= withdrawalAmount
+		fmt.Print("Money withdrawn! New balance: ", amount)
+
+	} else {
+		fmt.Print("Goodbye!")
+	}
+}n
