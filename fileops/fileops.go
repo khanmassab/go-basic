1495b08ea4ce4cc3a0bb1d7e69df68af6bfd37e3
Massab <massab725@gmail.com>
2024-11-25 15:49:59 +0500

    spacing

--- fileops/fileops.go
+++ fileops/fileops.go
@@ -12,17 +12,17 @@ func WriteBalanceToFile(input float64, fileName string){
 	os.WriteFile(fileName,[]byte(inputText), 0644)
 }
   
-func GetBalanceFromTheFile(fileName string) (float64, error){
-data, err := os.ReadFile(fileName)
+ func GetBalanceFromTheFile(fileName string) (float64, error){
+	data, err := os.ReadFile(fileName)
 
-if err != nil {
-	return 1000, errors.New("error finding the file")
-}
-valueText := string(data)
-value, err := strconv.ParseFloat(valueText, 64)
+	if err != nil {
+		return 1000, errors.New("error finding the file")
+	}
+	valueText := string(data)
+	value, err := strconv.ParseFloat(valueText, 64)
 
-if err != nil {
-	return 1000, errors.New("error reading from the file")
-}
-return value, nil
+	if err != nil {
+		return 1000, errors.New("error reading from the file")
+	}
+	return value, nil
 }
