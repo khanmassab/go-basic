0cbde2d6220df8381f0e3d6409f2f9635b2ee44c
Massab <massab725@gmail.com>
2024-11-26 15:14:42 +0500

    generic

--- app.go
+++ app.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+type TypeCaster[T any] struct {
+	status  bool
+	message string
+	data    T
+}
+
 func main() {
-	fmt.Print("Hello World")
+	variableTwo := TypeCaster[any]{status: true, message: "true", data: 8}
+	response := processData(variableTwo)
+	fmt.Print(response)
+
+}
+
+func processData [TypeCaster any] (response TypeCaster) TypeCaster {
+	return response
 }
