aa1b05a08733bdcb24a22264b443a03b7cc01aca
Massab <massab725@gmail.com>
2024-11-30 13:36:47 +0500

    slices and append method

--- arrays.go
+++ arrays.go
@@ -3,11 +3,23 @@ package main
 import "fmt"
 
 func main(){
+	prices := []float64{1, 2}
 
-	var stringArray [4]string = [4]string{"element1", "element2"}
-	var prices [4] float64 = [4] float64{64.0, 43.78, 453, 654}
+	newPrices := append(prices, 43)
 
-	stringArray[2] = "element3"
-	fmt.Println(prices[0:2]) // from index 0 and before index 2, i.e, 0 and 1
-	fmt.Println(stringArray[1])
+	fmt.Print(newPrices)
 }
+
+// func main(){
+
+// 	var stringArray [4]string = [4]string{"element1", "element2"}
+// 	var prices [4] float64 = [4] float64{64.0, 43.78, 453, 654}
+
+// 	var fromSecondToTheLastIndex = prices[1:]
+
+// 	fmt.Print(fromSecondToTheLastIndex)
+// 	stringArray[2] = "element3"
+// 	fmt.Println(prices[0:2]) // from index 0 and before index 2, i.e, 0 and 1
+// 	fmt.Println(stringArray[1])
+// 	fmt.Print(len(fromSecondToTheLastIndex), cap(fromSecondToTheLastIndex))
+// }
