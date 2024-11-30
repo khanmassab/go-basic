b86f73855632572da8b1407c164588d8d4fd518f
Massab <massab725@gmail.com>
2024-11-30 13:08:34 +0500

    slices

--- arrays.go
+++ arrays.go
@@ -5,8 +5,9 @@ import "fmt"
 func main(){
 
 	var stringArray [4]string = [4]string{"element1", "element2"}
+	var prices [4] float64 = [4] float64{64.0, 43.78, 453, 654}
 
 	stringArray[2] = "element3"
-
-	fmt.Print(stringArray[1])
+	fmt.Println(prices[0:2]) // from index 0 and before index 2, i.e, 0 and 1
+	fmt.Println(stringArray[1])
 }
